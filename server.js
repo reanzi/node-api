4ea90f667d465ecc0ccec372b2bdeef1c2caeba0
reanzi <nziku99@gmail.com>
2020-01-23 15:46:41 +0300

    basic routing ; get all, get single, update,create and  delete in a server.js file

--- server.js
+++ server.js
@@ -5,6 +5,30 @@ const dontenv = require("dotenv");
 dontenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Routes
+app.get("/api/v1/projects", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all projects" });
+});
+app.get("/api/v1/projects/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Get Single project ${req.params.id}` });
+});
+app.post("/api/v1/projects", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new project" });
+});
+app.put("/api/v1/projects/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update project ${req.params.id}` });
+});
+app.delete("/api/v1/projects/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete project ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(
   PORT,
