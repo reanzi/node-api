943abb3c0b8ab7ba62bdec87eff163ddddd4d93b
reanzi <nziku99@gmail.com>
2020-01-24 12:06:26 +0300

    Reading single data for db; done

--- controllers/projects.js
+++ controllers/projects.js
@@ -2,19 +2,35 @@ const Project = require("../models/Project");
 // @desc    Get all Projects
 // @router  GET /api/v1/projects
 // @access  Public
-exports.getProjects = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: "Show all projects", hello: req.hello });
+exports.getProjects = async (req, res, next) => {
+  try {
+    const projects = await Project.find();
+    res.status(200).json({
+      success: true,
+      data: projects
+    });
+  } catch (e) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    GEt single Project
 // @router  GET /api/v1/projects/:id
 // @access  Public
-exports.getProject = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Get Single project ${req.params.id}` });
+exports.getProject = async (req, res, next) => {
+  try {
+    const project = await Project.findById(req.params.id);
+
+    if (!project) {
+      return res.status(400).json({ success: false }); // return to avoid "header already set" Error
+    }
+    res.status(200).json({
+      success: true,
+      data: project
+    });
+  } catch (e) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Create new Project
