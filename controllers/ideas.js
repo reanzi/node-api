33e272e725d0d854248b926bbc6ee5bba8e05307
reanzi <nziku99@gmail.com>
2020-01-31 13:17:47 +0300

    Creating new Idea to a specific Project

--- controllers/ideas.js
+++ controllers/ideas.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require("../utils/ErrorResponse");
 const asyncHandler = require("../middleware/asyncHandler");
 const Idea = require("../models/Idea");
+const Project = require("../models/Project");
 
 // desc      Get ideas
 // @router  GET /api/v1/idea
@@ -24,5 +25,48 @@ exports.getIdeas = asyncHandler(async (req, res, next) => {
     success: true,
     count: ideas.length,
     data: ideas
+  });
+});
+
+// desc      Get Single idea
+// @router  GET /api/v1/idea/:id
+// @access  Public
+
+exports.getIdea = asyncHandler(async (req, res, next) => {
+  const idea = await Idea.findById(req.params.id).populate({
+    path: "project",
+    select: "name description"
+  });
+  if (!idea) {
+    return next(
+      new ErrorResponse(`No idea with the id of ${req.params.id}`),
+      404
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: idea
+  });
+});
+
+// desc      Create a idea
+// @router  POST /api/v1/projects/:projectID/ideas
+// @access  Private
+
+exports.addIdea = asyncHandler(async (req, res, next) => {
+  req.body.project = req.params.projectId;
+
+  const project = await Project.findById(req.params.projectId);
+  if (!project) {
+    return next(
+      new ErrorResponse(`No Project with the id of ${req.params.projectId}`),
+      404
+    );
+  }
+  const idea = await Idea.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: idea
   });
 });
--- routes/ideas.js
+++ routes/ideas.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getIdeas } = require("../controllers/ideas");
+const { getIdeas, getIdea, addIdea } = require("../controllers/ideas");
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(getIdeas);
+router
+  .route("/")
+  .get(getIdeas)
+  .post(addIdea);
+router.route("/:id").get(getIdea);
 
 module.exports = router;
