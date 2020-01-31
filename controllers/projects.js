296dea4156591f7fc8de89a7c596baca0105ce04
reanzi <nziku99@gmail.com>
2020-01-31 15:58:29 +0300

    image upload done, using file system storage

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const ErrorResponse = require("../utils/ErrorResponse");
 const geocoder = require("../utils/geocoder");
 const Project = require("../models/Project");
@@ -166,5 +167,59 @@ exports.getProjectsInRadius = asyncHandler(async (req, res, next) => {
     success: true,
     count: projects.length,
     data: projects
+  });
+});
+
+// @desc    Upload Photo for a Project
+// @router  DELETE /api/v1/projects/:id/photo
+// @access  Private
+exports.projectPhotoUpload = asyncHandler(async (req, res, next) => {
+  const project = await Project.findById(req.params.id);
+  if (!project) {
+    return next(
+      new ErrorResponse(`Project not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  if (!req.files) {
+    return next(new ErrorResponse(`Please Upload a file`, 400));
+  }
+
+  console.log(req.files);
+  const file = req.files.file;
+
+  //make sure image is a photo
+  if (!file.mimetype.startsWith("image")) {
+    return next(new ErrorResponse(`Please Upload an image file`, 400));
+  }
+
+  // check file size
+  if (file.size > process.env.MAX_FILE_UPLOAD_SIZE) {
+    return next(
+      new ErrorResponse(
+        `Please Upload an image less than ${process.env.MAX_FILE_UPLOAD_SIZE /
+          1000000}MB`,
+        400
+      )
+    );
+  }
+
+  //Custom file name
+  file.name = `photo_${project._id}${Math.random()}_${Date.now()}${
+    path.parse(file.name).ext
+  }`;
+
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
+    if (err) {
+      console.log(err);
+      return next(new ErrorResponse(`Problem with the file upload`, 500));
+    }
+    // Insert filename into the db
+    await Project.findByIdAndUpdate(req.params.id, { photo: file.name });
+
+    res.status(200).json({
+      success: true,
+      data: file.name
+    });
   });
 });
