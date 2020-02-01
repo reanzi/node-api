8bf441586425face3389d73bf19c7006b2002d92
reanzi <nziku99@gmail.com>
2020-02-01 14:32:39 +0300

    generating cookies and sending to the browser

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dontenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -25,6 +26,7 @@ const app = express();
  */
 //Body Parser
 app.use(express.json());
+app.use(cookieParser());
 
 // Dev logging Middleware
 if (process.env.NODE_ENV === "development") {
