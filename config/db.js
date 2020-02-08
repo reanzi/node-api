824cd2e88f100095017a21ebd03f8f65adbb0a3f
reanzi <nziku99@gmail.com>
2020-02-08 16:23:46 +0300

    modified db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
+  const conn = await mongoose.connect(process.env.MONGO_URI
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
